309c2fee06ce5d4a71c887af2303a77a5f40ed9e
Abanoub Nassar <104019469+AbanoubNassar@users.noreply.github.com>
2022-09-09 21:46:56 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+setInterval(setClock , 1000);
+
+const hourHand = document.querySelector('[data-hour-hand]');
+const minuteHand = document.querySelector('[data-minute-hand]');
+const secondHand = document.querySelector('[data-second-hand]');
+
+
+function setClock(){
+    const currentDate = new Date()
+    const secondsRatio = currentDate.getSeconds() /60
+    const minutesRatio = (secondsRatio + currentDate.getMinutes()) /60
+    const hoursRatio = (minutesRatio + currentDate.getHours()) /12
+
+    setRotation(secondHand, secondsRatio)
+    setRotation(minuteHand , minutesRatio)
+    setRotation(hourHand  , hoursRatio)
+    
+}
+
+
+function setRotation(element , rotationRatio){
+    element.style.setProperty('--rotation' , rotationRatio * 360)
+}
+
+let clock = document.querySelector(".clock");
+let hourBtn = document.getElementById("hourBtn");
+
+hourBtn.addEventListener("click", function changeColor(){
+    let color = document.getElementById('colorInputText').value;
+    clock.style.backgroundColor = color ;
+
+} )
+
